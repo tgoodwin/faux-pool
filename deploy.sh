ccf4383fa2398ccd36cf75da5cf1ed622b690a47
Tim Goodwin <timg.goodwin@gmail.com>
2018-03-20 23:30:15 -0400

    deploy typo

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ then
     echo "Which folder do you want to deploy to gh-pages?"
     exit 1
 fi
-echo "pushing "$1" subtree to gh-pages branch.
+echo "pushing "$1" subtree to gh-pages branch."
 git subtree push --prefix $1 origin gh-pages
