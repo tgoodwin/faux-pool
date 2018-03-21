30ef741cd447ef8e89a5af75d8b5fd1fa6b3357e
Tim Goodwin <timg.goodwin@gmail.com>
2018-03-20 23:33:44 -0400

    screw it simplify deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-if [-z "$1"]
-then
-    echo "Which folder do you want to deploy to gh-pages?"
-    exit 1
-fi
-echo "pushing "$1" subtree to gh-pages branch."
-git subtree push --prefix $1 origin gh-pages
+git subtree push --prefix dist origin gh-pages
